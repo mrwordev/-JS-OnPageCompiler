2e0915259a092f72371041df44ab9e6b0696933c
Woraprat Jindain <woraprat.j@aware.local>
2016-06-10 18:10:47 +0700

    Try to code without even run!

---
+++ WorCode.js
@@ -0,0 +1,208 @@
+var WorCode = (function() {
+    var interpreter = {
+        datatype: {
+            primary: {
+                "char": {
+                    maxlength: 1,
+                    accept: "text"
+                },
+                "int": {
+                    maxlength: 11,
+                    accept: "integer"
+                },
+                "float": {
+                    maxlength: 11,
+                    accept: "float"
+                },
+                "double": {
+                    maxlength: 11,
+                    accept: "float"
+                },
+                "string": {
+                    maxlength: 0,
+                    accept: "text"
+                },
+                "boolean": {
+                    maxlength: 0,
+                    accept: "boolean"
+                },
+            },
+            composite: ["array", "dictionary", "object"]
+        },
+        console: null,
+        code: "",
+        lines: null,
+        variables: {},
+        printQueue: [],
+        extract: function() {
+            var extracting = interpreter.code.split('\n');
+            if (extracting) {
+                interpreter.lines = extracting;
+            } else {
+                console.log("Code is empty.");
+            }
+        },
+        read: function() {
+            interpreter.extract();
+            var statements = [];
+            var isCondition = false;
+            var ifStatement = "";
+            for (var line in interpreter.lines) {
+                var code = interpreter.lines[line];
+                var _isPrimary = interpreter.isPrimary(code);
+                if (_isPrimary) {
+                    if (isCondition) {
+                        ifStatement += _isPrimary + ";";
+                    } else {
+                        statements.push(_isPrimary + ";");
+                    }
+                    continue;
+                }
+                var _isComposite = interpreter.isComposite(code);
+                if (_isComposite) {
+                    if (isCondition) {
+                        ifStatement += _isPrimary + ";";
+                    } else {
+                        statements.push(_isComposite + ";");
+                    }
+                    continue;
+                }
+                var _isVariable = interpreter.isVariable(code);
+                if (_isVariable) {
+                    if (isCondition) {
+                        ifStatement += _isVariable + ";";
+                    } else {
+                        statements.push(_isVariable + ";");
+                    }
+                    continue;
+                }
+                var _isPrint = interpreter.isPrint(code);
+                if (_isPrint) {
+                    statements.push(_isPrint + ";");
+                    continue;
+                } else {
+                    var regexCond = /(if|elseif|else|endif) \((.*)\)/g;
+                    var cond = regexCond.exec(code);
+                    if (cond) {
+                        if (cond[0] === "if") {
+                            isCondition.push(true);
+                            ifStatement += "if(" + cond[1] + "){";
+                        } else if (cond[0] === "elseif") {
+                            isCondition.push(true);
+                            ifStatement += "} else if(" + cond[1] + "){";
+                        } else if (cond[0] === "else") {
+                            isCondition.push(true);
+                            ifStatement += "} else {";
+                        } else if (cond[0] === "endif" && isCondition) {
+                            ifStatement += "}";
+                            statements.push(ifStatement);
+                            ifStatement = "";
+                        }
+                    }
+                }
+            }
+            interpreter.readStatement(statements);
+        },
+        isPrimary: function(code) {
+            var regexCode = /(\w+) (\W*|\w) (=) (.*)/g;
+            var extracted = regexCode.exec(code);
+            var types = interpreter.datatype.primary;
+            var validType = null;
+            if (extracted) {
+            	extracted.shift();
+                for (var type in types) {
+                    if (extracted[0] == type) {
+                        validType = types[type];
+                        break;
+                    }
+                }
+                if (validType) {
+                    extracted[0] = "var";
+                }
+                return extracted.join(" ");
+            }
+            return null;
+        },
+        isComposite: function(code) {
+            var regexCode = /(\w+) (\W*|\w) (=) (.*)/g;
+            var extracted = regexCode.exec(code);
+            var types = interpreter.datatype.composite;
+            var validType = null;
+            if (extracted) {
+            	extracted.shift();
+                for (var type in types) {
+                    if (extracted[0] == type) {
+                        validType = types[type];
+                        break;
+                    }
+                }
+                if (validType) {
+                    extracted[0] = "var";
+                }
+                return extracted.join(" ");
+            }
+            return null;
+        },
+        isVariable: function(code) {
+            var regexCode = /(\w+) (\W*|\w) (.*)/g;
+            var extracted = regexCode.exec(code);
+            if (extracted) {
+            	extracted.shift();
+                return extracted.join(" ");
+            }
+            return null;
+        },
+        isPrint: function(code) {
+            var regexCode = /(\w+) (.*)/g;
+            var extracted = regexCode.exec(code);
+            if (extracted) {
+            	extracted.shift();
+                if (extracted[0] === "print") return "$.print(" + extracted[1] + ")";
+            }
+            return null;
+        },
+        readStatement: function(lines) {
+            //var program = lines.join(";");
+            (function($, i, c) {
+                $ = $;
+                i = i;
+                for (var idx in c) {
+                    try {
+                        eval(c[idx]);
+                    } catch (e) {
+                        console.log(e);
+                    }
+                }
+            })($, interpreter, lines);
+        },
+    };
+    var $ = {
+        interpreter: null,
+        config: {
+            editor: null,
+            console: null,
+        },
+        init: function() {
+            $.interpreter = interpreter;
+        },
+        setEditor: function(element) {
+            $.config.editor = element;
+        },
+        setConsole: function(element) {
+            $.config.console = element;
+        },
+        execute: function() {
+            if ($.interpreter) {
+                $.interpreter.console = $.config.console;
+                $.interpreter.code = $.config.editor.value;
+                $.interpreter.read();
+            } else {
+                console.log("Intepreter is not define.")
+            }
+        },
+        print: function(text) {
+            $.config.console.innerHTML += "<br/>" + text;
+        },
+    };
+    return $;
+})();
